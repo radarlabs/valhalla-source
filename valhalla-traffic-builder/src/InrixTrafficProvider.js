53a418415abf9ec8621adbdebca77dca54199ea5
Boon Boonsiri <bboonsir@uwaterloo.ca>
2026-06-10 15:28:25 -0400

    temp

--- valhalla-traffic-builder/src/InrixTrafficProvider.js
+++ valhalla-traffic-builder/src/InrixTrafficProvider.js
@@ -132,17 +132,17 @@ class InrixTrafficProvider {
       let { code: segmentId, speed } = segment;
 
 
-      // Inrix Speed Adjustments
-      // Reference speed is freeflow speed at that datetime
-      if (speed / segment.reference <= 0.25) { // Inrix really tends to underestimate heavy heavy traffic. It knows where the traffic jams are, but doesn't make them cost enough 10km/h -> 6km/h
-        speed = speed * 0.6;
-      } else if (speed / segment.reference <= 0.4) { // Lighter version of above rule
-        speed = speed * 0.75;
-      } else if (segment.reference < 30 && speed / segment.reference < 0.75) { // Apply higher cost to traffic set in downtowns (where segment.reference is very slow)
-        speed = speed * 0.65;
-      } else if (segment.reference < 30) { // Inrix tends to overestimate speed in downtowns
-        speed = speed * 0.85;
-      }
+      // // Inrix Speed Adjustments
+      // // Reference speed is freeflow speed at that datetime
+      // if (speed / segment.reference <= 0.25) { // Inrix really tends to underestimate heavy heavy traffic. It knows where the traffic jams are, but doesn't make them cost enough 10km/h -> 6km/h
+      //   speed = speed * 0.6;
+      // } else if (speed / segment.reference <= 0.4) { // Lighter version of above rule
+      //   speed = speed * 0.75;
+      // } else if (segment.reference < 30 && speed / segment.reference < 0.75) { // Apply higher cost to traffic set in downtowns (where segment.reference is very slow)
+      //   speed = speed * 0.65;
+      // } else if (segment.reference < 30) { // Inrix tends to overestimate speed in downtowns
+      //   speed = speed * 0.85;
+      // }
 
       if (segment.segmentClosed) {
         speed = 0;
